1e2fd222362a10ed72c27d879707221580db3a3f
Dasha240196 <ostalcovadasha@mail.ru>
2015-12-11 16:00:10 +0300

    zadanie7

---
+++ Unit1.pas
@@ -0,0 +1,57 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Registry;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var RegIni: TRegIniFile ;
+begin
+RegIni:=TRegIniFile.Create('Soft');
+RegIni.WriteString('hello','Param',Edit1.Text);
+//if not RegIni.OpenKey('Software\Hello', false) then
+    //Label1.Caption:='добавлено';
+    Label1.Caption:=RegIni.ReadString('hello','Param','Добавлено');
+RegIni.Free;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var RegIni: TRegistry ;
+
+begin
+RegIni:=TRegistry.Create;
+RegIni.RootKey:=HKEY_CURRENT_USER;
+if  RegIni.DeleteKey('Soft\hello') then
+  label1.Caption:='удалено';
+RegIni.Free;
+
+
+end;
+
+end.
+
+
+
